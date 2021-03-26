42dec27b18bb398eb96e660f84c071861ab8b98f
Văn Khánh Vương <vankhanhvuong98@gmail.com>
2021-03-26 09:53:16 +0700

    Initial Commit

---
+++ W2-CustomUIView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  W2-CustomUIView
+//
+//  Created by Văn Khánh Vương on 26/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ W2-CustomUIViewTests/W2_CustomUIViewTests.swift
@@ -0,0 +1,33 @@
+//
+//  W2_CustomUIViewTests.swift
+//  W2-CustomUIViewTests
+//
+//  Created by Văn Khánh Vương on 26/03/2021.
+//
+
+import XCTest
+@testable import W2_CustomUIView
+
+class W2_CustomUIViewTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
